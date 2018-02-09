7ff1247a37d71cce06e2ff6ea7c6501f5314e10f
AhmedAbd-Allah <35036845+AhmedAbd-Allah@users.noreply.github.com>
2018-02-09 23:59:42 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -19,10 +19,8 @@ from books import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/',include('books.urls'))
+    path('books/',include('books.urls')),
+    path('ratings/', include('star_ratings.urls', namespace='ratings'))
 ]
 
 
-if settings.DEBUG:
-    urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
